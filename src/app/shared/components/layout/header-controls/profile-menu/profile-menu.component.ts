f477e3c748358b1cb9c187fbf1dd225065feb9b4
Jonathan Navarro <jonathan.d.navarro.v@gmail.com>
2025-12-29 18:41:20 -0300

    Ajustar z-index del header y mejorar Select, accesibilidad y estados disabled

--- src/app/shared/components/layout/header-controls/profile-menu/profile-menu.component.ts
+++ src/app/shared/components/layout/header-controls/profile-menu/profile-menu.component.ts
@@ -1,7 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CloseOnInteractDirective } from '../../../../directives/close-on-interact.directive';
+import { IconComponent } from '../../../ui/icon/icon.component';
 
 export interface ProfileMenuItem {
   label: string;
@@ -11,7 +12,7 @@ export interface ProfileMenuItem {
 
 @Component({
   selector: 'app-profile-menu',
-  imports: [CommonModule, CloseOnInteractDirective, RouterLink],
+  imports: [CommonModule, IconComponent, CloseOnInteractDirective, RouterLink],
   templateUrl: './profile-menu.component.html',
   styleUrl: './profile-menu.component.css',
 })
