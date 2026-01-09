b05188859a8f6fe61c26d729177094fc220d9a7a
Jonathan Navarro <jonathan.d.navarro.v@gmail.com>
2026-01-09 18:45:13 -0300

    Separar template de modal-host en archivo .html

--- src/app/shared/components/ui/modal/modal-host.component.ts
+++ src/app/shared/components/ui/modal/modal-host.component.ts
@@ -2,30 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, TemplateRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ModalComponent } from './modal.component';
-import { ModalStackService, ModalStackItem } from './modal-stack.service';
+import { ModalStackService, ModalStackItem } from '../../../../core/services/modal-stack.service';
 
 @Component({
   selector: 'app-modal-host',
   standalone: true,
   imports: [CommonModule, ModalComponent],
-  template: `
-    <ng-container *ngIf="current$ | async as item">
-      <app-modal
-        [title]="item.title || ''"
-        [buttons]="item.buttons"
-        [showBack]="((stack$ | async)?.length || 0) > 1"
-        (back)="modalStack.back()"
-        (closed)="modalStack.back()"
-        (action)="handleAction($event)"
-      >
-        <ng-container *ngIf="item.template">
-          <ng-container
-            *ngTemplateOutlet="item.template; context: { data: item.data }"
-          ></ng-container>
-        </ng-container>
-      </app-modal>
-    </ng-container>
-  `,
+  templateUrl: './modal-host.component.html',
 })
 export class ModalHostComponent {
   current$: Observable<ModalStackItem | null>;
